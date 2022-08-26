fbd19725537020ac9897c9b1debc48ef70cede81
shyamenk <shyamenk@gmail.com>
2022-08-27 02:16:49 +0400

    new file:   part1/src/component/Content.jsx 	new file:   part1/src/component/Header.jsx 	new file:   part1/src/component/Part.jsx 	new file:   part1/src/component/Total.jsx

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,5 +1,34 @@
+import Content from './component/Content'
+import Header from './component/Header'
+import Total from './component/Total'
+
 const App = () => {
-  return <div></div>
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  }
+  return (
+    <div>
+      <div>
+        <Header course={course} />
+        <Content course={course} />
+        <Total course={course} />
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ part1/src/component/Total.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Total = props => {
+  return <p>Number of exercises </p>
+}
+
+export default Total
